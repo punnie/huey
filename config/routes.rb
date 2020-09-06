23fa484d1a6e3d2e4533c86a3ae1e407003340c3
Pedro Coelho <punnie@ethereal.io>
2020-09-06 19:29:46 +0100

    Add API endpoint routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users, only: []
+
+  namespace :api do
+    namespace :v2 do
+      resources :aggregations do
+        resources :entries, only: [:index]
+      end
+
+      resources :feeds do
+        resources :entries, only: [:index]
+      end
+
+      resources :subscriptions
+    end
+  end
 end
