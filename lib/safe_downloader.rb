bf44b8b3b40e5629658c23605a0df32e06da69a7
Pedro Coelho <punnie@ethereal.io>
2021-02-02 10:18:00 +0000

    Improve SafeDownloader

--- lib/safe_downloader.rb
+++ lib/safe_downloader.rb
@@ -1,18 +1,23 @@
+# typed: false
 # frozen_string_literal: true
 
 class SafeDownloader
-  USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:80.0) Gecko/20100101 Firefox/80.0'
-  MAX_SIZE = 10 * 1024 * 1024
+  attr_reader :max_size, :max_hops
 
-  def safe_download(url, max_hops: 30)
+  def initialize(max_size: 10 * 1024 * 1024, max_hops: 30)
+    @max_size = max_size
+    @max_hops = max_hops
+  end
+
+  def download(url)
     content = String.new
     content_size = 0
 
-    response = HTTP.headers(user_agent: USER_AGENT)
+    response = HTTP.headers(user_agent: user_agent)
       .follow(max_hops: max_hops)
       .get(url)
 
-    while content_size < MAX_SIZE
+    while content_size < max_size
       chunk = response.body.readpartial
       break if chunk.nil?
 
@@ -21,5 +26,27 @@ class SafeDownloader
     end
 
     content
+  end
+
+  private
+
+  def user_agent
+    [
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.1; rv:85.0) Gecko/20100101 Firefox/85.0',
+      'Mozilla/5.0 (X11; Linux i686; rv:85.0) Gecko/20100101 Firefox/85.0',
+      'Mozilla/5.0 (Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0',
+      'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:85.0) Gecko/20100101 Firefox/85.0',
+      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0',
+      'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36 Edg/88.0.705.56',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36 Edg/88.0.705.56',
+    ].sample
   end
 end
