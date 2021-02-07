e7c0180c3ac34be09b1c986ce10f9e17195a0075
Pedro Coelho <punnie@ethereal.io>
2021-02-07 18:15:07 +0000

    Remove unused endpoints

--- app/controllers/api/v2/feeds_controller.rb
+++ app/controllers/api/v2/feeds_controller.rb
@@ -4,14 +4,12 @@
 class Api::V2::FeedsController < ApplicationController
   before_action :set_feed, only: %i[show]
 
-  # GET /api/v2/feeds/1
   # GET /api/v2/feeds/1.json
   # GET /api/v2/feeds/1.rss
   def show
     @entries = @feed.entries.order(published_date: :desc).limit(10)
 
     respond_to do |format|
-      format.html
       format.json { render layout: false }
       format.rss { render layout: false }
     end
