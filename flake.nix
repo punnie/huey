1377ec7d5007881f9d93c54d22ac6ea8e4f8452a
Pedro Coelho <punnie@ethereal.io>
2023-01-13 15:53:36 +0000

    Get build up and running for assets

--- flake.nix
+++ flake.nix
@@ -22,20 +22,57 @@
           gemset   = ./gemset.nix;
         };
 
-        hueyPackage = pkgs.stdenv.mkDerivation {
+        hueyPackage = pkgs.stdenv.mkDerivation rec {
           name = "huey";
 
           src = self;
 
-          buildInputs = [ pkgs.cacert hueyGems hueyGems.wrappedRuby ];
+          hueyModules = pkgs.stdenv.mkDerivation {
+            name = "huey-modules";
+
+            inherit src;
+
+            yarnOfflineCache = pkgs.fetchYarnDeps {
+              yarnLock = ./yarn.lock;
+              sha256 = "sha256-oQMVZK0v8qyxCGrCo3cMAPWECFnE1IkjMBP4h+zsPYg=";
+            };
+
+            nativeBuildInputs = [ (with pkgs; [ fixup_yarn_lock nodejs yarn ]) hueyGems hueyGems.wrappedRuby ];
+
+            RAILS_ENV = "production";
+            NODE_ENV = "production";
+
+            buildPhase = ''
+              export HOME=$PWD
+
+              fixup_yarn_lock $HOME/yarn.lock
+              yarn config --offline set yarn-offline-mirror $yarnOfflineCache
+              yarn install --offline --frozen-lockfile --ignore-engines --ignore-scripts --no-progress
+
+              patchShebangs $HOME/bin
+              patchShebangs $HOME/node_modules
+
+              PATH="$HOME/bin:$PATH" SECRET_KEY_BASE=precompile_placeholder rails assets:precompile
+              yarn cache clean --offline
+              rm -rf $HOME/node_modules/.cache
+            '';
+
+            installPhase = ''
+              mkdir -p $out/public
+              cp -rv $HOME/public/assets $out/public
+            '';
+          };
+
+          buildInputs = [ pkgs.cacert pkgs.nodejs-slim hueyGems hueyGems.wrappedRuby ];
 
           buildPhase = ''
             patchShebangs bin/
+            cp -rv ${hueyModules}/public/assets public
           '';
 
           installPhase = ''
             mkdir -p $out/app
-            cp -rv $src/* $out/app
+            cp -rv * $out/app
           '';
         };
 
