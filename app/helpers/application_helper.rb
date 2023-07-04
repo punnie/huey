cfc447e75488eb15939ed4850c36fe3cafac97ca
Pedro Coelho <punnie@ethereal.io>
2023-07-04 06:39:15 +0000

    Improve and simplify logic around random avatars

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+  AVATAR_STYLES = %w[marble beam pixel sunset ring bauhaus].freeze
+
+  def img_src_for_entry(entry)
+    if entry.readable_content && entry.readable_content.dig('table', 'lead_image_url').present?
+      entry.readable_content.dig('table', 'lead_image_url')
+    else
+      "https://source.boringavatars.com/#{AVATAR_STYLES[entry.feed.id % AVATAR_STYLES.length]}/120/#{entry.feed.id}?colors=264653,2a9d8f,e9c46a,f4a261,e76f51&square=true"
+    end
+  end
+end
