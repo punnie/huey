c69e50d90dbef8cfcb0a138cdf62f262775e06b9
Pedro Coelho <punnie@ethereal.io>
2023-10-14 00:23:02 +0100

    Update nixpkgs to 23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Huey flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
