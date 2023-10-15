df45449058a414f283df6e7e28fd0a3e2ad34072
Pedro Coelho <punnie@ethereal.io>
2023-10-15 22:58:19 +0100

    Allow for entry layout customization

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -4,6 +4,7 @@ class StreamsController < ApplicationController
   def show
     respond_to do |format|
       format.html { render layout: 'layouts/application' }
+      format.js { render layout: false }
     end
   end
 
@@ -12,18 +13,33 @@ class StreamsController < ApplicationController
   def set_controller_entities
     @streams = Stream.all.sorted
 
-    @stream = Stream.find(params[:id])
+    @stream = Stream.find(filtered_params[:id])
 
     @feeds = @stream.feeds
 
     @entries = @stream.entries.for_feed.includes(:feed).page(filtered_params[:page]).per(default_per_page)
+
+    @entry_layout = entry_layout
   end
 
   def default_per_page
     filtered_params[:per_page] || 100
   end
 
+  def entry_layout
+    case filtered_params[:l]
+    when 'full'
+      'entry'
+    when 'cozy'
+      'entry_cozy'
+    when 'compact'
+      'entry_compact'
+    else
+      'entry_cozy'
+    end
+  end
+
   def filtered_params
-    params.slice(:page)
+    params.permit(:id, :page, :per_page, :js, :l)
   end
 end
