986cb175808599296066acf4de3a62b53d5e0901
Pedro Coelho <punnie@ethereal.io>
2024-03-16 14:03:15 +0000

    Remove old api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :api do
-    namespace :v2 do
-      resources :feeds, only: [:show]
-    end
-
-    namespace :v3 do
-      resources :feeds
-      resources :streams
-      resources :stream_assignments, only: [:create, :destroy]
-      resources :entries, only: [:index]
-    end
-  end
-
   resources :feeds, only: [:show]
   resources :streams, only: [:show]
 
-  root to: 'welcome#index'
+  root to: 'welcome#reader'
+
   get 'chronological', to: 'welcome#chronological', as: :chronological
   get 'reader', to: 'welcome#reader', as: :reader
 end
